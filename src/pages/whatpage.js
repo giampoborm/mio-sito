57e990123c191e2ef79d74801810bd06d989bc51
giampoborm <90754684+giampoborm@users.noreply.github.com>
2025-07-17 11:26:24 +0200

    Refine cursor rules

--- src/pages/whatpage.js
+++ src/pages/whatpage.js
@@ -4,10 +4,15 @@ export function renderWhatpage(app) {
   // Clear the main app container.
   app.innerHTML = '';
 
+  document.body.classList.add('what-page');
+
   // Initialize the what page physics simulation.
   // This function returns a teardown function.
   const teardownWhatPhysics = setupWhatPhysics();
 
   // Return the teardown function so that the router can call it when leaving this page.
-  return teardownWhatPhysics;
+  return () => {
+    document.body.classList.remove('what-page');
+    teardownWhatPhysics();
+  };
 }
